fe5fe2645616808bfe28216f9c700f7455032948
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2024-05-27 17:46:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
     install_requires=[
         'aiohttp>=3.8.1',  # For asynchronous HTTP requests
         'python-dateutil>=2.8.2',  # For parsing dates
-        'requests>=2.26.0',  # For fetching the JSON feed
-        # 'exorde_data',  # If this is a custom module, ensure it is installable or included in the package
+        'requests>=2.26.0'  # For fetching the JSON feed
     ],
     extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
 )
